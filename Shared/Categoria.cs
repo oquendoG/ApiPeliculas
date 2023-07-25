b9c5a5b095db966fff7f89c4159e83ee5da421ff
Wilson Oquendo <oquendo999@hotmail.com>
2023-07-25 08:30:42 -0500

    Add project files.

---
+++ Shared/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Shared;
+
+public class Categoria
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public DateTimeOffset CreationDate { get; set; } = DateTimeOffset.UtcNow;
+}
