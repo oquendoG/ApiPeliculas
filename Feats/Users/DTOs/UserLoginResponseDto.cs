e71af5a10a6ba43f420b68c32f06d2fb1dd903f2
Wilson Oquendo <oquendo999@hotmail.com>
2023-09-07 08:23:07 -0500

    feat: añadidos modelos y dtos de usuario

---
+++ Feats/Users/DTOs/UserLoginResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using ApiPeliculas.Shared;
+
+namespace ApiPeliculas.Feats.Users.DTOs;
+
+/// <summary>
+/// Respuesta del servidor al autenticarse
+/// </summary>
+public class UserLoginResponseDto
+{
+    public required User User { get; set; }
+    public string Token { get; set; }
+}
